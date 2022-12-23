cf374fc59a6a7cc4aa28eda1c36b086a43927147
jdsafhlfl <weiyi.xu@helsinki.fi>
2022-12-23 15:01:07 +0200

    exercise1.2

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -3,16 +3,14 @@ const App = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
+  const exercises2 = 17
   const part3 = 'State of a component'
   const exercises3 = 24
 
   return (
     <div>
       <Header course={course} />
-      <Content part_name={part1} exercise_num={exercises1} />
-      <Content part_name={part2} exercise_num={exercises2} />
-      <Content part_name={part3} exercise_num={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total num={exercises1+exercises2+exercises3} />
     </div>
   )
@@ -27,6 +25,16 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    return (
+      <div>
+        <Part part_name={props.part1} exercise_num={props.exercises1} />
+        <Part part_name={props.part2} exercise_num={props.exercises2} />
+        <Part part_name={props.part3} exercise_num={props.exercises3} />        
+      </div>
+    )
+}
+
+const Part = (props) => {
   return (
     <div>
       <p>{props.part_name} {props.exercise_num}</p>
